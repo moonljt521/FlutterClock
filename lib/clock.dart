8baa0df749207331acf40af8d0133221332b9a78
jiangtao.liang <446741151@qq.com>
2026-01-12 17:11:42 +0800

    [fx]升级sdk，添加动画 eg

--- lib/clock.dart
+++ lib/clock.dart
@@ -3,12 +3,32 @@ import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+// 数字时钟 Widget
+class DigitalClock extends StatelessWidget {
+  final DateTime time;
+
+  const DigitalClock({Key? key, required this.time}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      "${time.hour.toString().padLeft(2, '0')}:${time.minute.toString().padLeft(2, '0')}:${time.second.toString().padLeft(2, '0')}",
+      style: TextStyle(
+        fontSize: 48,
+        fontWeight: FontWeight.bold,
+        color: Colors.black87,
+        letterSpacing: 4,
+      ),
+    );
+  }
+}
+
 class Clock extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => ClockState();
 }
 
-class ClockState extends State<Clock> {
+class ClockState extends State<Clock> with TickerProviderStateMixin {
 
   final _divider = Container(width: 25);
 
@@ -17,13 +37,20 @@ class ClockState extends State<Clock> {
   var timeout = const Duration(seconds: 1);
 
   DateTime today = new DateTime.now();
+  DateTime previousTime = new DateTime.now();
 
   bool _isDispose = false;
 
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+
   _startTimer() {
     Timer.periodic(timeout, (timer) {
-      today = new DateTime.now();
-      setState(() {});
+      setState(() {
+        previousTime = today;
+        today = new DateTime.now();
+      });
+      _animationController.forward(from: 0);
       if(_isDispose){
         timer.cancel();
       }
@@ -33,6 +60,14 @@ class ClockState extends State<Clock> {
   @override
   void initState() {
     super.initState();
+    _animationController = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    );
+    _animation = CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeInOut,
+    );
     _startTimer();
   }
 
@@ -42,59 +77,72 @@ class ClockState extends State<Clock> {
     var prefix = [0, 1, 2, 3, 4, 5];
     var suffix = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-    DateTime today = new DateTime.now();
-    print("当前时间 ： ${today.hour} ： ${today.minute} ： ${today.second}");
-
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text("Flutter Clock"),
+        title: Text("Creative Clock"),
       ),
-      body: Container(
+      body: SingleChildScrollView(
+        child: Container(
           alignment: Alignment.center,
-          child: Row(
-            // crossAxisAlignment: CrossAxisAlignment.start,
+          padding: EdgeInsets.symmetric(vertical: 20),
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              // 数字时钟显示
+              DigitalClock(time: today),
+              SizedBox(height: 40),
+              // 动画时钟
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
 
-              _columnNumber(hourPrefix, today.hour ~/ 10),
-              _columnNumber(suffix, today.hour % 10),
+                  _columnNumber(hourPrefix, today.hour ~/ 10, previousTime.hour ~/ 10),
+                  _columnNumber(suffix, today.hour % 10, previousTime.hour % 10),
 
-              _divider,
+                  _divider,
 
-              _columnNumber(prefix, today.minute ~/ 10),
-              _columnNumber(suffix, today.minute % 10),
+                  _columnNumber(prefix, today.minute ~/ 10, previousTime.minute ~/ 10),
+                  _columnNumber(suffix, today.minute % 10, previousTime.minute % 10),
 
-              _divider,
+                  _divider,
 
-              _columnNumber(prefix, today.second ~/ 10),
-              _columnNumber(suffix, today.second % 10),
+                  _columnNumber(prefix, today.second ~/ 10, previousTime.second ~/ 10),
+                  _columnNumber(suffix, today.second % 10, previousTime.second % 10),
+                ],
+              ),
             ],
           )),
+      ),
       // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 
-  _columnNumber(List<int> numbers, int current) {
+  _columnNumber(List<int> numbers, int current, int previous) {
     List<Widget> list = [];
     numbers.forEach((e) {
       list.add(_Number(e, e == current, numbers.length));
     });
 
-    return Container(
-      // color: Colors.white,
-      child: Align(
-        alignment: Alignment.topCenter,
-        widthFactor: 1,
-        heightFactor: (current *2 + 1)/numbers.length,
-        child: Container(
-          height: numbers.length * _normalSize,
-          margin: EdgeInsets.only(left: 5, right: 5),
-          child: Column(
-            children: list,
-          )),
-      ),
+    return AnimatedBuilder(
+      animation: _animation,
+      builder: (context, child) {
+        // 计算动画插值
+        double animatedPosition = previous + (current - previous) * _animation.value;
+        
+        return Container(
+          child: Align(
+            alignment: Alignment.topCenter,
+            widthFactor: 1,
+            heightFactor: (animatedPosition * 2 + 1) / numbers.length,
+            child: Container(
+              height: numbers.length * _normalSize,
+              margin: EdgeInsets.only(left: 5, right: 5),
+              child: Column(
+                children: list,
+              )),
+          ),
+        );
+      },
     );
   }
 
@@ -137,6 +185,7 @@ class ClockState extends State<Clock> {
 
   @override
   void dispose() {
+    _animationController.dispose();
     super.dispose();
     _isDispose = true;
   }
